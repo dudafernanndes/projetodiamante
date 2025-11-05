b8788932a2beb7e65ea05a72e2c77f005e544168
dudafernanndes <dev.dudafernandes@gmail.com>
2025-11-05 14:58:58 -0300

    inicializando

---
+++ src/main/java/br/fiap/diamante/DiamanteApplication.java
@@ -0,0 +1,13 @@
+package br.fiap.diamante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiamanteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiamanteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/fiap/diamante/DiamanteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.fiap.diamante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiamanteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
